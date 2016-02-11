d77ada1d4d27c1554a58d57c91ae6e458cb717d9
Pierre-Henri Coustenoble <ph.coustenoble@gmail.com>
2016-02-11 10:57:44 +0100

    added routes

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ActiveRecord::Base
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail, uniqueness: true
+  validates :ingredient, uniqueness: true
 end
